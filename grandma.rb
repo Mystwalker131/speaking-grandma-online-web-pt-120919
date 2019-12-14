685889f0e8b08cfe501590847fc361c8e6ad286b
Benito Galvez <mystwalker131@gmail.com>
2019-12-14 01:25:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,10 +3,10 @@ def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
    return "I LOVE YOU TOO PUMPKIN!"
    
-   elseif string != string.upcase
+   else if string != string.upcase
    return "HUH?! SPEAK UP, SONNY!"
    
-   elseif string == string.upcase
+   else if string == string.upcase
    return "NO, NOT SINCE 1938!"
  
  end
